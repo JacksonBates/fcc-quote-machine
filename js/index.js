ef0db521ca29cd3ba7e9072a6312857466d0a475
JacksonBates <malgalin@gmail.com>
2016-07-11 09:16:54 +1000

    initial pages commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  
+  $("#getQuote").on("click", function() {
+    // Only change code below this line.
+    $.ajax({
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      url: "http://api.forismatic.com/api/1.0/",
+      contentType: "application/jsonp",
+      data: {
+        lang: "en",
+        method: "getQuote",
+        format: 'jsonp'
+      },
+      success: function(data) {
+        $("#quoteText").text(data.quoteText);
+        if (data.quoteAuthor !== "") {
+          $("#quoteAuthor").text(data.quoteAuthor);
+          $("#tweet-button").attr("href","https://twitter.com/intent/tweet?text=" + data.quoteText + " " + data.quoteAuthor);
+        } else {
+          $("#quoteAuthor").text("Unknown");
+          $("#tweet-button").attr("href", "https://twitter.com/intent/tweet?text=" + data.quoteText + " " + data.quoteAuthor);
+  }
+        
+      },
+      error: function(data) {
+        $("#quoteText").text('-');
+        $("#quoteText").text('An error has occurred');
+      }
+      
+    });
+    
+    // Only change code above this line.
+  });
+})
